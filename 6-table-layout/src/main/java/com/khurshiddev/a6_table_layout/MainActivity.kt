dbb6fe7f29f629c501788f36f48b44e2dd189f6d
khurshiddev <umaribnxattob8008@gmail.com>
2023-03-20 13:44:43 +0900

    [add::] table-layout

---
+++ 6-table-layout/src/main/java/com/khurshiddev/a6_table_layout/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.khurshiddev.a6_table_layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_page_with_table_layout)
+    }
+}
