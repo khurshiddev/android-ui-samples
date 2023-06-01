d9bf91f8f126ae433303bcfb9130b8df0a8e4f3c
khurshiddev <umaribnxattob8008@gmail.com>
2023-06-01 21:17:48 +0900

    feat: add multiple-choice list

--- 21-alert-dialog/src/main/java/com/example/a21_alert_dialog/MainActivity.kt
+++ 21-alert-dialog/src/main/java/com/example/a21_alert_dialog/MainActivity.kt
@@ -1,39 +1,39 @@
 package com.example.a21_alert_dialog
 
+
 import android.content.DialogInterface
+import android.content.DialogInterface.OnClickListener
+import android.content.DialogInterface.OnMultiChoiceClickListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.example.a21_alert_dialog.databinding.ActivityMainBinding
 
-
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val checkedList = BooleanArray(3) // bu shu checked listni o'zida save qiladi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.dialogBtn.setOnClickListener {
+            val list = arrayOf("Firs", "Second", "Third")
             val dialog = AlertDialog.Builder(this)
-                .setTitle("Exit")
-                .setIcon(R.drawable.baseline_warning_24)
-                //.setPositiveButtonIcon(getDrawable(R.drawable.baseline_warning_24)) //
-                .setMessage("Do you want to exit the application?")
-                .setPositiveButton("Ok",object :DialogInterface.OnClickListener{
-                    override fun onClick(dialog: DialogInterface?, which: Int) {
-                        finish()
+                .setTitle("Choose one")
+                .setCancelable(false) // bu ekranni boshqa joyini bosganda chiqib ketmasili uchun
+                .setMultiChoiceItems(list, checkedList,object : OnMultiChoiceClickListener{
+                    override fun onClick(dialog: DialogInterface?, position: Int, isChecked: Boolean) {
+                        Log.d("dialog","$position is $isChecked")
+                        checkedList[position] = isChecked  // bu qaysi biri checked bo'ganini save qilib turadi
                     }
                 })
-                .setNegativeButton("Cancel",object :DialogInterface.OnClickListener{
-                    override fun onClick(dialog: DialogInterface?, which: Int) {
-                        Toast.makeText(this@MainActivity, "Cancel button clicked",Toast.LENGTH_SHORT).show()
-
-                    }
-                })
+                .setPositiveButton("OK") { _, _ -> }
+                .setNegativeButton("CANCEL") { _, _ -> }
                 .create()
-                dialog.show()
+            dialog.show()
         }
     }
 }
