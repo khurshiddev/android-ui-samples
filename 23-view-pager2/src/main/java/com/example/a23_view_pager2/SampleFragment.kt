bf38624ff489570b6e0ad08463761911662f4bd8
khurshiddev <umaribnxattob8008@gmail.com>
2023-06-04 21:20:42 +0900

    feat: refactor

--- 23-view-pager2/src/main/java/com/example/a23_view_pager2/SampleFragment.kt
+++ 23-view-pager2/src/main/java/com/example/a23_view_pager2/SampleFragment.kt
@@ -10,26 +10,30 @@ import com.example.a23_view_pager2.databinding.SampleFragmentLayoutBinding
 class SampleFragment() : Fragment() {
 
     private var _binding: SampleFragmentLayoutBinding? = null
-    private  val binding get() = _binding!!
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = SampleFragmentLayoutBinding.inflate(layoutInflater,container,false)
+        _binding = SampleFragmentLayoutBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.textView.text = arguments?.let {
-            it.getString("key")
-        }
+        val imageRes = arguments?.let {
+            it.getInt("key")
+        } ?: R.drawable.apple
+        binding.imageView.setImageResource(imageRes)
+
     }
+
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
 }
+
