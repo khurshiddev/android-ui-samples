3eb84fdeee6bcb5751f6bc59a9b7b8a25fc47066
khurshiddev <umaribnxattob8008@gmail.com>
2023-06-14 21:48:01 +0900

    refactor: refactor

--- 23-view-pager2/src/main/java/com/example/a23_view_pager2/CustomDataModel.kt
+++
@@ -1,4 +0,0 @@
-package com.example.a23_view_pager2
-
-class CustomDataModel(val icon:Int,val image:Int,val title:String) {
-}
--- 23-view-pager2/src/main/java/com/example/a23_view_pager2/CustomVIewPagerAdapter.kt
+++ 23-view-pager2/src/main/java/com/example/a23_view_pager2/CustomVIewPagerAdapter.kt
@@ -5,19 +5,16 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class CustomVIewPagerAdapter(activity: FragmentActivity, val list: List<Int>) :
-    FragmentStateAdapter(activity) {
+class CustomViewPagerAdapter(activity: FragmentActivity):FragmentStateAdapter(activity) {
     override fun getItemCount(): Int {
-        return list.size
+        return 10
     }
 
     override fun createFragment(position: Int): Fragment {
         return SampleFragment().apply {
             arguments = Bundle().apply {
-                putInt("key", list[position])
+                putString("key",position.toString())
             }
         }
     }
-
-
 }
